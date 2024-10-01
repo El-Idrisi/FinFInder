114505e63cda6f66debf37a9671d3b12d286c9b5
El-Idrisi <muhammad.3l.idrisi@gmail.com>
2024-10-01 09:05:06 +0700

    menambahkan tailwindcss

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <h1>Hello World!</h1>
+</body>
+
+</html>
