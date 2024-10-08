d62e7286868552aa5ac106e6a4dbdd9309a42571
El-Idrisi <muhammad.3l.idrisi@gmail.com>
2024-10-08 11:53:54 +0700

    membuat migration user, fish_spot, fish_type

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'nelayan']);
+            $table->string('nama')->nullable();
+            $table->string('no_telp')->nullable();
+            $table->text('alamat')->nullable();
+            $table->date('tanggal_lahir')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
