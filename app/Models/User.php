525435b2fc8f3e5ede75b10660002105ad61825a
El-Idrisi <muhammad.3l.idrisi@gmail.com>
2024-10-09 09:52:26 +0700

    menyempurnakan desain pada halaman login

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,9 +17,14 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'username',
         'email',
         'password',
+        'role',
+        'nama',
+        'no_telp',
+        'alamat',
+        'tanggal_lahir',
     ];
 
     /**
