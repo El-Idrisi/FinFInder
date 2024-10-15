f245abfc7690dc4f62be22a696d9d4958e264cc1
El-Idrisi <muhammad.3l.idrisi@gmail.com>
2024-10-15 23:32:09 +0700

    membuat sidebar bisa berfungsi dalam tampilan mobile dan tablet

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -17,7 +17,8 @@
 
 <body class="relative font-inter -z-[9999] overflow-x-hidden bg-sky-50">
 
-    <aside id="sidebar" class="h-full w-[240px] bg-white transition-all duration-300 fixed top-0 bottom-0 shadow-lg">
+    <aside id="sidebar"
+        class="-translate-x-full lg:translate-x-0 h-full w-[240px] bg-white transition-all duration-300 fixed top-0 bottom-0 shadow-lg">
         <div class="px-12 py-4">
             <img src="{{ asset('img/finfinder.png') }}" alt="logo finfinder">
         </div>
@@ -29,7 +30,7 @@
                     <img src="{{ asset('img/sidebar/Group 133.svg') }}" alt="dashboard-icon" class="scale-125">
                     Dashboards
                 </a>
-                <a href="/" class="flex items-center gap-4 transition-all duration-300 hover:text-sky-500" >
+                <a href="/" class="flex items-center gap-4 transition-all duration-300 hover:text-sky-500">
                     <img src="{{ asset('img/sidebar/Group 134.svg') }}" alt="dashboard-icon" class="scale-125">
                     Home
                 </a>
@@ -43,7 +44,7 @@
                     <img src="{{ asset('img/sidebar/Group 136.svg') }}" alt="dashboard-icon" class="scale-125">
                     Data Ikan
                 </a>
-                <a href="#" class="flex items-center gap-4 transition-all duration-300 hover:text-sky-500" >
+                <a href="#" class="flex items-center gap-4 transition-all duration-300 hover:text-sky-500">
                     <img src="{{ asset('img/sidebar/Group 137.svg') }}" alt="dashboard-icon" class="scale-125">
                     Your Data
                 </a>
@@ -61,18 +62,15 @@
         </div>
 
         <div class="px-4 mt-8 font-bold">
-            <a href="#" class="px-[3.5rem] py-2 text-white bg-gradient-to-r from-violet-400    via-sky-500 to-fuchsia-400 rounded-md bg-size-200 bg-pos-0 transition-all duration-300 hover:bg-pos-100">
+            <a href="#"
+                class="px-[3.5rem] py-2 text-white bg-gradient-to-r from-violet-400    via-sky-500 to-fuchsia-400 rounded-md bg-size-200 bg-pos-0 transition-all duration-300 hover:bg-pos-100">
                 <span class="shad"><i class="fa-solid fa-plus"></i> Add Data</span>
             </a>
         </div>
 
     </aside>
-    {{-- <div class="flex w-full h-full">
 
-
-    </div> --}}
-
-    <div class="content-shifted ml-[240px]" id="content">
+    <div class="content-shifted lg:ml-[240px]" id="content">
         <header class="flex justify-between w-full gap-12 px-8 text-lg bg-white shadow-lg h-fit">
             <div class="py-6">
                 <a href="#" id="hambuger"><i class="fa-solid fa-bars"></i></a>
@@ -85,7 +83,8 @@
                     <i class="text-lg transition-transform duration-300 fa-solid fa-chevron-down"></i>
                 </button>
 
-                <div class="absolute flex flex-col overflow-hidden text-lg transition-all duration-300 bg-white top-[4.7rem] accordion-content shadow-lg rounded-b-lg">
+                <div
+                    class="absolute flex flex-col overflow-hidden text-lg transition-all duration-300 bg-white top-[4.7rem] accordion-content shadow-lg rounded-b-lg">
                     <a href="" class="block w-full px-8 py-2 transition-all duration-300 hover:bg-sky-300"><i
                             class="fa-solid fa-gear"></i>
                         Settings</a>
@@ -116,14 +115,12 @@
         accordions.forEach(acc => {
             acc.addEventListener('click', function() {
                 var panel = this.nextElementSibling;
-                console.log(panel)
                 if (panel.style.maxHeight) {
                     panel.style.maxHeight = null;
                     this.classList.toggle('active')
                 } else {
                     panel.style.maxHeight = panel.scrollHeight + 'px';
                     this.classList.toggle('active')
-                    // this.classList.remove('active')
                 }
             });
         });
@@ -132,11 +129,27 @@
         const content = document.querySelector('#content');
         const hambuger = document.querySelector("#hambuger")
 
-        hambuger.addEventListener('click', () => {
-            sidebar.classList.toggle("-translate-x-full")
-            content.classList.toggle("ml-[240px]")
-            hambuger.classList.toggle("hamburger-active")
-        });
+        if (window.innerWidth <= 768) {
+            hambuger.addEventListener('click', (event) => {
+                event.stopPropagation();
+                sidebar.classList.remove("-translate-x-full");
+                sidebar.classList.add("translate-x-0");
+            });
+
+            document.addEventListener('click', (event) => {
+                if (!sidebar.contains(event.target) && !hambuger.contains(event.target)) {
+                    sidebar.classList.remove("translate-x-0");
+                    sidebar.classList.add("-translate-x-full");
+                }
+            });
+
+        } else {
+            hambuger.addEventListener('click', () => {
+                sidebar.classList.toggle("lg:-translate-x-full")
+                sidebar.classList.toggle("-translate-x-full")
+                content.classList.toggle("lg:ml-[240px]")
+            });
+        }
     </script>
     @stack('script')
 </body>
